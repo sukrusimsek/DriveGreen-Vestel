0447cf1ac2bab68dd0649840f5e64c9c00beb122
sukrusimsek <sukrusimsekll@gmail.com>
2024-01-21 13:52:05 +0300

    first commit

---
+++ DriveGreen-Vestel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DriveGreen-Vestel
+//
+//  Created by Şükrü Şimşek on 21.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
